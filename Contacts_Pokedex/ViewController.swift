51a00190c5080791864447051ef72763f0bc03eb
Kieran <shadowfox@outlook.sg>
2019-06-27 09:15:52 +0800

    Initial Commit

---
+++ Contacts_Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Contacts_Pokedex
+//
+//  Created by Kieran Ng on 27/6/19.
+//  Copyright © 2019 Kieran Ng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
